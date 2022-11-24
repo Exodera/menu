a421438248b61ffb769062f25d4da8b549db1431
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-24 14:04:33 +0200

    git init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  return(
+  <main>
+    <div className='container'>
+      <h3 className='title'>Our Menu</h3>
+      <div className='underline'></div>
+
+    </div>
+
+  </main>
+  )
+}
+
+export default App;
