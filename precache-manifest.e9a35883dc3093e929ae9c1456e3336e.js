854b8186538bb6196c0701d7efd85ced1a511c93
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-24 16:58:13 +0200

    Updates

---
+++ precache-manifest.e9a35883dc3093e929ae9c1456e3336e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1645a3134a056ddba2e3fc0e6a5a1539",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "7087f100c5406a727c02",
+    "url": "/menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "fe10914c32aad6d9862e",
+    "url": "/menu/static/js/2.c609c091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.c609c091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7087f100c5406a727c02",
+    "url": "/menu/static/js/main.0a0b0bea.chunk.js"
+  },
+  {
+    "revision": "56ef0208991cca3c548d",
+    "url": "/menu/static/js/runtime-main.a5a74f46.js"
+  },
+  {
+    "revision": "8b929915439a8d1ce388da6f91488118",
+    "url": "/menu/static/media/item-1.8b929915.jpeg"
+  },
+  {
+    "revision": "6cb0297249af1e9edbeeacf702cd8b85",
+    "url": "/menu/static/media/item-10.6cb02972.jpeg"
+  },
+  {
+    "revision": "9900a731aa92beb3604918ec1ad11526",
+    "url": "/menu/static/media/item-2.9900a731.jpeg"
+  },
+  {
+    "revision": "f6fffc793ba9ca89c0a8a50ce6960a66",
+    "url": "/menu/static/media/item-3.f6fffc79.jpeg"
+  },
+  {
+    "revision": "fdf5de3d48fd9563036ce3a5e83fa779",
+    "url": "/menu/static/media/item-4.fdf5de3d.jpeg"
+  },
+  {
+    "revision": "68ad10f1a59f00591db8d80831c67e59",
+    "url": "/menu/static/media/item-5.68ad10f1.jpeg"
+  },
+  {
+    "revision": "f62370a35f8f32530478f42e73a60f60",
+    "url": "/menu/static/media/item-6.f62370a3.jpeg"
+  },
+  {
+    "revision": "6a9cad582f29f822bf661c221a9cebb7",
+    "url": "/menu/static/media/item-7.6a9cad58.jpeg"
+  },
+  {
+    "revision": "28b2ec1c05cd81f3da9a6bc21bb688e0",
+    "url": "/menu/static/media/item-8.28b2ec1c.jpeg"
+  },
+  {
+    "revision": "38b6a11ed25d1ad54b95dd763555834f",
+    "url": "/menu/static/media/item-9.38b6a11e.jpeg"
+  }
+]);
