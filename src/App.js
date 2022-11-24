359bf8c425094ed6a12ba797a7a79f40c5dabcb1
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-24 16:55:48 +0200

    image bug update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) =>{
+    if(category==='All'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item)=>item.category === category);
+    setMenuItems(newItems);
+  }
+
   return(
   <main>
-    <div className='container'>
+    <div className='menu container'>
       <h3 className='title'>Our Menu</h3>
       <div className='underline'></div>
+      <Categories filterItems={filterItems}/>
+      <Menu items={menuItems}/>
+
 
     </div>
 
